365e17d19cca4afbae42c7b589e790f615a813fc
Fillip Mangia <fillipgms@gmail.com>
2024-02-03 18:45:23 -0300

    setttings page update

--- app/(protected)/settings/page.tsx
+++ app/(protected)/settings/page.tsx
@@ -4,6 +4,7 @@ import { Button } from "@/components/ui/button";
 import { useCurrentUser } from "@/hooks/useCurrentUser";
 import { setCart } from "@/redux/reducer/cartReducer";
 import { signOut } from "next-auth/react";
+import Image from "next/image";
 import { useDispatch } from "react-redux";
 import { MoonLoader } from "react-spinners";
 
@@ -25,8 +26,24 @@ const SettingsPage = () => {
     }
 
     return (
-        <main>
-            <Button onClick={onClick}>sair</Button>
+        <main className="max-w-2xl mx-auto">
+            <div className="w-full space-y-2 flex flex-col items-center justify-center py-5">
+                <Image
+                    height={100}
+                    width={100}
+                    className="rounded-full shadow-md"
+                    alt="sua foto de perfil"
+                    src={
+                        user.image ||
+                        "https://upload.wikimedia.org/wikipedia/commons/a/ac/Default_pfp.jpg"
+                    }
+                />
+                <h2 className="font-semibold">{user.name}</h2>
+            </div>
+            <div className=" text-center">
+                <h3 className="font-semibold">Meus pedidos</h3>
+                <div></div>
+            </div>
         </main>
     );
 };
